aeb82535ff6c88af158bc21b6a7b650a852eed6b
Takumi Adachi <takumiadachi@gmail.com>
2019-03-22 07:50:59 -0700

    Show committer, branch, date, and sha on render.

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,50 @@
-class Greetings extends React.Component {
+/**
+ * See https://developer.github.com/v3/repos/branches/#get-branch
+ *
+ * Example Github api request:
+ * https://api.github.com/repos/ta-dachi/eatsleepcode.tech/branches/master
+ */
+class LatestCommitComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      author: "",
+      branch: "",
+      date: "",
+      sha: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://api.github.com/repos/ta-dachi/eatsleepcode.tech/branches/master"
+    )
+      .then(response => {
+        response.json().then(json => {
+          console.log(json);
+          this.setState({
+            author: json.commit.author.login,
+            branch: json.name,
+            date: json.commit.commit.author.date,
+            sha: json.commit.sha
+          });
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   render() {
-    return <h1>Greetings, {this.props.name}!</h1>;
+    return (
+      <div>
+        <div>{this.state.author}</div>
+        <div>{this.state.branch}</div>
+        <div>{this.state.date}</div>
+        <div>{this.state.sha}</div>
+      </div>
+    );
   }
 }
-console.log("test");
-ReactDOM.render(<Greetings name="Chris" />, document.getElementById("test"));
+
+ReactDOM.render(<LatestCommitComponent />, document.getElementById("root"));
